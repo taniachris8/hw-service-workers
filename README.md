489c78b4d3c32ad8e67be8a44dd55c7dcba25dd7
taniachris8 <dove6464dove@gmail.com>
2025-11-16 12:16:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WebWorkers, ServiceWorkers
+
+![CI](https://github.com/taniachris8/hw-service-workers/actions/workflows/web.yml/badge.svg)
+
+[Link to GitHub Pages](https://taniachris8.github.io/hw-service-workers/)
---
+++ src/js/News.js
@@ -0,0 +1,31 @@
+export default class News {
+  constructor(parentEl, date, image, title) {
+    this.parentEl = parentEl;
+    this.date = date;
+    this.image = image;
+    this.title = title;
+  }
+
+  bindToDOM() {
+    this.news = document.createElement("div");
+    this.news.classList.add("news");
+    this.news.innerHTML = `
+        <time class="date">${this.formatDate()}</time>
+        <div class="news-wrapper">
+        <img alt="News image" src=${this.image} class="news-image">
+        <p class="news-title">${this.title}</p>
+        </div>
+        `;
+  }
+
+  formatDate() {
+    const date = new Date(this.date);
+    const dateStr = date.toLocaleDateString("ru-RU");
+    const timeStr = date.toLocaleTimeString("ru-RU", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+    return `${timeStr} ${dateStr}`;
+  }
+}
